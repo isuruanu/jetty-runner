e16e1e101431ca540cad2bd048a0dd71691265d3
isuruanu <isuru.develop@gmail.com>
2014-12-12 18:22:14 +0600

    compilation issue fixed for 14IC

--- src/main/java/com/github/guikeller/jettyrunner/runner/JettyProgramDebugger.java
+++ src/main/java/com/github/guikeller/jettyrunner/runner/JettyProgramDebugger.java
@@ -43,7 +43,6 @@ public class JettyProgramDebugger extends GenericDebuggerRunner {
     }
 
     @Nullable
-    @Override
     protected RunContentDescriptor createContentDescriptor(Project project, RunProfileState state, RunContentDescriptor contentToReuse, ExecutionEnvironment env)
                 throws ExecutionException {
         // Now we figure out if it the Debug button has been hit
@@ -62,10 +61,10 @@ public class JettyProgramDebugger extends GenericDebuggerRunner {
             // Creating a 'Remote' configuration on the fly
             RemoteConnection connection = new RemoteConnection(true, LOCALHOST, debuggerPort, false);
             // Attaches the remote configuration to the VM and then starts it up
-            return attachVirtualMachine(project, state, contentToReuse, env, connection, true);
+            return attachVirtualMachine(state, env, connection, true);
         }else{
             // If it was something else then we don't do anything special
-            return super.createContentDescriptor(project,state,contentToReuse,env);
+            return super.createContentDescriptor(state, env);
         }
     }
 
